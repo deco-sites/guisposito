d161bff5866078e3b82696770e3a1294b198af2e
Guisposito <guicarlosjoinhas@gmail.com>
2024-02-19 18:06:34 -0300

    Corrigindo componente title SEO

--- sections/Content/TitleSEO.tsx
+++ sections/Content/TitleSEO.tsx
@@ -1,14 +1,14 @@
-export interface Props{
-    title?: string;
-    activeTitle: boolean;
-    placement?: "Left" | "Center";
-    fontSize?: number;
+export interface Props {
+  title?: string;
+  activeTitle: boolean;
+  placement?: "Left" | "Center";
+  fontSize?: number;
 }
 
 export default function TitleComp({
   title,
   activeTitle,
-  placement = 'Center',
+  placement = "Center",
   fontSize,
 }: Props) {
   let align;
@@ -25,9 +25,13 @@ export default function TitleComp({
   }
   return (
     <div>
-      <div className={`mx-auto flex flex-col items-${align} gap-8 sm:py-10 px-6`}>
+      <div
+        className={`mx-auto flex flex-col items-${align} gap-8 sm:py-10 px-6`}
+      >
         {activeTitle && (
-          <h1 className={`inline-block text-[${fontSize}px] leading-100% font-medium tracking--2.4px`}>
+          <h1
+            className={`inline-block text-[${fontSize}px] leading-100% font-medium tracking--2.4px`}
+          >
             {title}
           </h1>
         )}
